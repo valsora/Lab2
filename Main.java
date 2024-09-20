9016e95be9c948a26b87db54f4316e4d0ac93ef4
valsora <valsora9@gmail.com>
2024-09-20 08:44:30 +0300

    changed main for tests

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(GeometricFigure.getNumberOfCreated());
         Ball ball1 = new Ball();
-        System.out.println(ball1.getNumberOfCreated());
+        System.out.println(GeometricFigure.getNumberOfCreated());
         Cylinder cylinder1 = new Cylinder();
-        System.out.println(ball1.getNumberOfCreated());
-        System.out.println(cylinder1.getNumberOfCreated());
+        System.out.println(GeometricFigure.getNumberOfCreated());
+        System.out.println(GeometricFigure.getNumberOfCreated());
+        cylinder1.setHeight(4);
         ball1.setRadius(9);
         System.out.println(ball1.getRadius());
-        System.out.println(ball1.calculateVolume());
+        System.out.println(cylinder1.calculateVolume());
         Parallelepiped p = new Parallelepiped(0, 0, 0, 10, 10, Math.PI / 2, 6, Math.PI / 6, Math.PI / 6);
         System.out.println(p.calculateSurfaceSquare());
         System.out.println(p.calculateVolume());
